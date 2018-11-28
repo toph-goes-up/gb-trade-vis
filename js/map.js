71423b26b6e4700ade101ac04e59648b4e950b8d
tophski <talkingtikihead@gmail.com>
2018-11-27 18:57:33 -0700

    Updated map.

--- js/map.js
+++ js/map.js
@@ -7,6 +7,7 @@ class Map {
         this.projection = d3.geoNaturalEarth1()
             .rotate([-3.44, -55.37]);
         this.geoGen = d3.geoPath().projection(this.projection);
+        this.selected = null;
     }
 
     draw(world) {
@@ -19,6 +20,18 @@ class Map {
         // Draw graticule
         map.select("#grat").append("path")
             .attr('d', this.geoGen(d3.geoGraticule()()));
+
+        // Draw empty globe
+        let globe = {
+            "type": "Sphere",
+            "coordinates": [
+                [[-180, -90], [-180, 90], [180, 90], [180, -90], [-180, -90]]
+            ]
+        };
+        map.select("#globe")
+            .append("path")
+            .attr('d', this.geoGen(globe))
+            .style("fill", "#9bbff4");
 
         // Draw continents
         d3.json("./data/continents.json").then(continents => {
@@ -37,14 +50,76 @@ class Map {
                 .append("path")
                 .attr("id", d => d.id)
                 .classed("colony", true)
-                .attr('d', this.geoGen);
+                .attr('d', this.geoGen)
+                .on("mouseover", this.handleMouseOver)
+                .on("mouseout", this.handleMouseOut)
+                .on("click", this.selectColony);
         });
-
-        this.drawTradeLinks(world);
-    }
-
-    drawTradeLinks(world){
-        console.log(world);
         console.log(window.dataSet)
+        this.drawTradeLinks();
     }
+
+    drawTradeLinks() {
+        d3.csv("./data/country_centroids_az8.csv", d => {
+            d.latitude = +d.Latitude;
+            d.longitude = +d.Longitude;
+            d.id = d["adm0_a3_is"];
+
+            return d;
+        }).then(centroids => {
+            // Use properties of centroids: id, latitude, longitude.
+
+            // Filter to relevant countries
+            let isos = window.dataSet.map(x => x.key);
+            centroids = centroids.filter(x => isos.includes(x.id));
+
+            // Shortcut for the GBR item
+            let gbr = centroids.find(x => x.id === "GBR");
+
+            // Create arc pairs
+            let arcs = centroids.map(x => {
+                return {
+                    id: x.id,
+                    type: "LineString",
+                    coordinates: [
+                        [x.longitude, x.latitude],
+                        [gbr.longitude, gbr.latitude]
+                    ]
+                }
+            });
+
+            d3.select("#trade-arcs")
+                .selectAll("path")
+                .data(arcs)
+                .enter()
+                .append("path")
+                .attr('id', d => {return d.id + "-arc"})
+                .attr('d', this.geoGen)
+                .classed("trade-arcs", true)
+                .on("mouseover", this.handleMouseOver)
+                .on("mouseout", this.handleMouseOut)
+                .on("click", this.selectColony);
+        });
+    }
+
+    // Highlight a country on mouse out of the arc or feature
+    handleMouseOver(d, i){
+        d3.select('#' + d.id).classed("colony-hover", true);
+        d3.select('#' + d.id + "-arc").classed("arc-hover", true);
+    };
+
+    // Un-highlight a country on mouse out of the arc or feature
+    handleMouseOut(d, i){
+        d3.select('#' + d.id).classed("colony-hover", false);
+        d3.select('#' + d.id + "-arc").classed("arc-hover", false);
+    };
+
+    // Select the clicked colony.
+    selectColony(d, i) {
+        console.log(d.id)
+        let toggled = !d3.select("#" + d.id).classed("colony-selected");
+        d3.selectAll(".colony").classed("colony-selected", false);
+        d3.select("#" + d.id).classed("colony-selected", toggled);
+    }
+
 }
